8c1d12abe4755befe5d23241d05b186db8eed1d9
Chris De Sa <cdesa@stanford.edu>
2014-04-16 15:12:33 -0700

    Fixed bug

--- benchmark/report.py
+++ benchmark/report.py
@@ -87,6 +87,19 @@ def main():
   report_data = [loadData(h, apps, args.verbose) for h in report_hashes]
 
   # generate the plots
+  for aa in ac_apps:
+    vc_plot = {}
+    vc_plot["title"] = "{0} Performance Comparison".format(" vs ".join(a.name for a in aa))
+    vc_plot["series"] = [a.name for a in aa]
+    vc_plot["data"] = []
+    for c in aa[0].configs:
+      cc_data = {}
+      cc_data["xlabel"] = c.name
+      for a in aa:
+        cc_data[a.name] = "{0}".format(report_data[0][a.name + "/" + c.name])
+      vc_plot["data"].append(cc_data)
+    plot_data.append(vc_plot)
+
   plot_data = []
   for app in vc_apps:
     vc_plot = {}
@@ -97,20 +110,7 @@ def main():
       cc_data = {}
       cc_data["xlabel"] = c.name
       for (h, d) in zip(report_hashes, report_data):
-        cc_data[h] = "{0:.4f}".format(d[app.name + "/" + c.name])
-      vc_plot["data"].append(cc_data)
-    plot_data.append(vc_plot)
-
-  for aa in ac_apps:
-    vc_plot = {}
-    vc_plot["title"] = "{0} Performance Comparison".format(" vs ".join(a.name for a in aa))
-    vc_plot["series"] = [a.name for a in aa]
-    vc_plot["data"] = []
-    for c in aa[0].configs:
-      cc_data = {}
-      cc_data["xlabel"] = c.name
-      for a in aa:
-        cc_data[a.name] = "{0:.4f}".format(report_data[0][a.name + "/" + c.name])
+        cc_data[h] = "{0}".format(d[app.name + "/" + c.name])
       vc_plot["data"].append(cc_data)
     plot_data.append(vc_plot)
 
