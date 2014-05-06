e4edc6b63b2632f0bfc2d8273f3b6d29189fee0c
Christopher De Sa <cdesa@stanford.edu>
2014-05-05 17:22:21 -0700

    Added CPP generation flag

--- benchmark/config.py
+++ benchmark/config.py
@@ -28,7 +28,7 @@ class App(object):
     self.delitec_options = delitec_options
 
   def stage_command(self):
-    return "bin/delitec %s %s" % (self.delitec_options, self.runner_class)
+    return "bin/delitec -v --cpp %s %s" % (self.delitec_options, self.runner_class)
 
   def run_command(self, c, runs, verbose):
     if(c.run_only_once):
