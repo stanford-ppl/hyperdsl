197208bc87955182dbdf570b834ba1d0ee1c0d91
Christopher De Sa <cdesa@stanford.edu>
2014-05-19 15:26:26 -0700

    Changed benchmark script to send output to /kunle directory, where it will be independent of the current hyperdsl installation.  Output is now written to a JSON file for display in a browser.

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -6,22 +6,145 @@ import os
 import argparse
 import subprocess
 import time
+import json
+import socket
+import re
 
+import util
 import config
 
+ansi_escape = re.compile(r'\x1b[^m]*m')
 
 def main():
   parser = argparse.ArgumentParser(description="Gather performance numbers for Delite apps.")
   parser.add_argument("-v", "--verbose", action="store_true")
   parser.add_argument("-f", "--force", action="store_true",
     help="force execution even if the git repository contains uncommitted changes")
-  parser.add_argument("-r", "--runs", type=int, default="10",
+  parser.add_argument("-r", "--runs", type=int, default="5",
     help="number of times to run the apps")
-  parser.add_argument("-s", "--skip-report", action="store_true", help="skip generating a report")
-  parser.add_argument("-b", "--bars", type=int, default="3", help="number of bars to display in graphs")
+  parser.add_argument("-p", "--filtered-runs", type=int, default="2",
+    help="filter the first this many runs")
+  parser.add_argument("-d", "--directory", type=str, default="/kunle/ppl/delite/benchmark/times",
+    help="directory to write the raw output into")
+  parser.add_argument("-j", "--json-directory", type=str, default="/kunle/ppl/delite/benchmark/json",
+    help="directory to write the json output into")
+  parser.add_argument("-s", "--skip-runs", action="store_true",
+    help="skip all process calls")
+  parser.add_argument("apps", type=str, nargs="*", default=config.default_apps, help="apps to run")
 
   args = parser.parse_args()
 
+  # collect the apps to run
+  apps = []
+  dsls = []
+  for a in args.apps:
+    if (a not in config.apps):
+      print("error: app {0} not found in config file".format(a), file=sys.stderr)
+      exit(-1)
+    app = config.apps[a]
+    if(app.dsl not in dsls):
+      dsls.append(app.dsl)
+      if(args.verbose):
+        print("notice: identified dsl {0}".format(app.dsl.name))
+    apps.append(app)
+    if(args.verbose):
+      print("notice: identified app {0}".format(app.name))
+
+  # chdir to the hyperdsl root directory
+  hyperdsl_root = util.chdir_hyperdsl_root()
+
+  # check that there are no changes to the repository
+  git_status = subprocess.check_output("git status -s", shell=True)
+  if((not args.force) and (git_status != "")):
+    print("error: hyperdsl repository contains uncommitted changes", file=sys.stderr)
+    print("       commit these changes before running benchmark.py", file=sys.stderr)
+    exit(-1)
+
+  # identify the hash associated with the current branch
+  git_hash = subprocess.check_output("git rev-parse --short HEAD", shell=True).strip()
+  if(args.verbose):
+    print("notice: identified git hash {0}".format(git_hash), file=sys.stderr)
+
+  if(args.verbose):
+    print("notice: creating directory for experimental results", file=sys.stderr)
+  subprocess.check_call("rm -f {0}/latest".format(args.directory), shell=True)
+  subprocess.check_call("mkdir -p {0}/{1}".format(args.directory, git_hash), shell=True)
+  subprocess.check_call("ln -s {0} {1}/latest".format(git_hash, args.directory), shell=True)
+
+  output_json = {}
+  output_json["git_hash"] = git_hash
+  output_json["start_time"] = time.time()
+  output_json["host"] = socket.gethostname()
+  output_json["dsls"] = {}
+  output_json["apps"] = {}
+  output_json["app_names"] = []
+
+  if(args.verbose):
+    print("notice: publishing forge dsls", file=sys.stderr)
+  for dsl in dsls:
+    if dsl.needs_publish:
+      if(args.verbose):
+        print("notice: publishing {0}".format(dsl.name), file=sys.stderr)
+      output_json["dsls"][dsl.name] = json_call(dsl.publish_command, 
+        "{0}/{1}/{2}.publish".format(args.directory, git_hash, dsl.name), args.skip_runs)
+
+  if(args.verbose):
+    print("notice: running apps", file=sys.stderr)
+  for app in apps:
+    output_json["app_names"].append(app.name)
+    output_json["apps"][app.name] = {}
+    if(args.verbose):
+      print("notice: staging {0}".format(app.name), file=sys.stderr)
+    os.chdir(app.dsl.run_dir)
+    output_json["apps"][app.name]["stage"] = json_call(app.stage_command(), 
+      "{0}/{1}/{2}.delitec".format(args.directory, git_hash, app.name), args.skip_runs)
+    output_json["apps"][app.name]["configs"] = []
+    output_json["apps"][app.name]["runs"] = {}
+    for c in app.configs:
+      output_json["apps"][app.name]["configs"].append(c.name)
+      if(args.verbose):
+        print("notice: running {0} under configuration {1}".format(app.name, c.name))
+      opts = " -Dstats.dump -Dstats.dump.component=app -Dstats.dump.overwrite -Dstats.output.dir={0}/{1} -Dstats.output.filename={2}-{3}.times {4}".format(
+        args.directory, git_hash, app.name, c.name, os.getenv("JAVA_OPTS", ""))
+      os.putenv("JAVA_OPTS", opts)
+      output_json["apps"][app.name]["runs"][c.name] = json_call(app.run_command(c, args.runs, args.verbose),
+        "{0}/{1}/{2}-{3}.delite".format(args.directory, git_hash, app.name, c.name), args.skip_runs)
+      output_json["apps"][app.name]["runs"][c.name]["opts"] = opts
+      cafn = "{0}/{1}/{2}-{3}.times".format(args.directory, git_hash, app.name, c.name)
+      if(os.path.isfile(cafn)):
+        with open(cafn, "r") as ftimes:
+          raw_times = [float(t)*1e-6 for t in ftimes.read().strip().split("\n")]
+        output_json["apps"][app.name]["runs"][c.name]["raw_times"] = raw_times
+        if (c.run_only_once):
+          if(len(raw_times) == 1):
+            output_json["apps"][app.name]["runs"][c.name]["filtered_times"] = raw_times
+            output_json["apps"][app.name]["runs"][c.name]["avg_time"] = raw_times[0]
+          else:
+            output_json["apps"][app.name]["runs"][c.name]["filtered_times"] = []
+            output_json["apps"][app.name]["runs"][c.name]["avg_time"] = 0.0
+        else:
+          if(len(raw_times) == args.runs):
+            filtered_times = raw_times[args.filtered_runs:]
+            output_json["apps"][app.name]["runs"][c.name]["filtered_times"] = filtered_times
+            output_json["apps"][app.name]["runs"][c.name]["avg_time"] = sum(filtered_times)/(len(filtered_times) + 1e-60)
+          else:
+            output_json["apps"][app.name]["runs"][c.name]["filtered_times"] = []
+            output_json["apps"][app.name]["runs"][c.name]["avg_time"] = 0.0
+      else:
+        output_json["apps"][app.name]["runs"][c.name]["raw_times"] = []
+        output_json["apps"][app.name]["runs"][c.name]["filtered_times"] = []
+        output_json["apps"][app.name]["runs"][c.name]["avg_time"] = 0.0
+    os.chdir(hyperdsl_root)
+
+  output_json["end_time"] = time.time()
+  output_json["total_time"] = output_json["end_time"] - output_json["start_time"]
+  if(args.verbose):
+    print("notice: ran for {0} seconds".format(output_json["total_time"]))
+  json.dump(output_json, open("{0}/{1}.json".format(args.json_directory, git_hash), "w"))
+
+
+
+def chdir_hyperdsl_root():
   # first, change the current working directory to the hyperdsl root
   hyperdsl_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
   if(os.path.abspath(os.getcwd()) != hyperdsl_root):
@@ -43,282 +166,17 @@ def main():
     print("error: unable to call git.", file=sys.stderr)
     exit(-1)
 
-  # check that there are no changes to the repository
-  git_status = subprocess.check_output("git status -s", shell=True)
-  if((not args.force) and (git_status != "")):
-    print("error: hyperdsl repository contains uncommitted changes", file=sys.stderr)
-    print("       commit these changes before running benchmark.py", file=sys.stderr)
-    exit(-1)
-
-  # identify the hash associated with the current branch
-  git_hash = subprocess.check_output("git rev-parse --short HEAD", shell=True).strip()
-  if(args.verbose):
-    print("notice: identified git hash {0}".format(git_hash), file=sys.stderr)
-
-  if(args.verbose):
-    print("notice: creating directory for experimental results", file=sys.stderr)
-  subprocess.check_call("mkdir -p benchmark/times/{0}".format(git_hash), shell=True)
-
-  if(args.verbose):
-    print("notice: compiling dsls", file=sys.stderr)
-  for dsl in config.dsls:
-    if(args.verbose):
-      print("notice: compiling {0}".format(dsl.name), file=sys.stderr)
-    subprocess.check_call(dsl.update_command(), stdout=sys.stderr, stderr=sys.stderr, shell=True)
-
-  delite_options = "-r {0}".format(args.runs)
-  if(args.verbose):
-    delite_options += " -v"
-
-  if(args.verbose):
-    print("notice: running apps", file=sys.stderr)
-  for app in config.apps:
-    if(args.verbose):
-      print("notice: staging {0}".format(app.name), file=sys.stderr)
-    os.chdir("published/" + app.dsl.name)
-    subprocess.check_call(app.stage_command(), stdout=sys.stderr, stderr=sys.stderr, shell=True)
-    for c in app.configs:
-      if(args.verbose):
-        print("notice: running {0} under configuration {1}".format(app.name, c.name))
-      opts = " -Dstats.dump -Dstats.dump.component=app -Dstats.dump.overwrite -Dstats.output.dir={0} -Dstats.output.filename={1}-{2}.times {3}".format(
-        hyperdsl_root + "/benchmark/times/" + git_hash, app.name, c.name, os.getenv("JAVA_OPTS", ""))
-      os.putenv("JAVA_OPTS", opts)
-      subprocess.check_call(app.run_command(c, delite_options), stdout=sys.stderr, stderr=sys.stderr, shell=True)
-    os.chdir(hyperdsl_root)
-
-  if(args.skip_report):
-    return 0
-
-  # identify the most recent timed hashes
-  if(args.verbose):
-    print("notice: searching for previous times", file=sys.stderr)
-  report_hashes = []
-  git_log = subprocess.check_output("git log --pretty=format:%h", shell=True).strip().split("\n")
-  for gh in git_log:
-    if(os.path.isdir("benchmark/times/" + gh)):
-      if(args.verbose):
-        print("notice: identified timed hash " + gh, file=sys.sterrr)
-      report_hashes.append(gh)
-      if(len(report_hashes) >= args.bars):
-        break
-  else:
-    if(args.verbose):
-      print("notice: ran out of previous hashes", file=sys.sterrr)
-
-  if(git_hash not in report_hashes):
-    print("error: couldn't find data for the runs we just did")
-
-  # load data for all hashes
-  report_data = [loadData(h, args.verbose) for h in report_hashes]
-
-  # write out the report
-  with open("benchmark/times/{0}/report.html".format(git_hash), "w") as freport:
-    # write the report head
-    print(report_head.format(git_hash), file=freport)
-    # write the report title
-    print("  <h1>Performance Comparison for Commit [{0}]</h1>".format(git_hash), file=freport)
-    print("  <p>Plots generated at {0}.</p>".format(time.strftime("%c")), file=freport)
-    # start writing out the plots
-    print("  <script>", file=freport)
-    for app in config.apps:
-      print("    makePerformancePlot([", file=freport)
-      for c in app.configs:
-        rs = "      {{ \"Configuration\": \"{0}\", ".format(c.name)
-        rs += ", ".join("\"{0}\": \"{1:.4f}\"".format(h, v[app.name + "/" + c.name]) for (h, v) in zip(report_hashes, report_data))
-        rs += " },"
-        print(rs, file=freport)
-      print("    ], \"{0}/{1} Performance Comparison\");".format(app.dsl.name, app.name), file=freport)
-    #finish the plots
-    print("  </script>", file=freport)
-    # and close the tags
-    print("</body>", file=freport)
-    print("</html>", file=freport)
+  return hyperdsl_root
 
 
-def loadData(git_hash, verbose):
-  if(verbose):
-    print("notice: loading data for hash " + git_hash, file=sys.stderr)
+def json_call(command, file_pfx, skip_runs):
   rv = {}
-  for app in config.apps:
-    for c in app.configs:
-      cafn = "benchmark/times/{0}/{1}-{2}.times".format(git_hash, app.name, c.name)
-      if(os.path.isfile(cafn)):
-        with open(cafn, "r") as f:
-          catimes = f.read().strip().split("\n")
-          # discard the first half of the entries
-          catimes = catimes[(len(catimes) // 2):]         
-          rv[app.name + "/" + c.name] = sum(float(t)*1e-6 for t in catimes)/len(catimes)
-      else:
-        rv[app.name + "/" + c.name] = 0
+  rv["command"] = command
+  if(not skip_runs):
+    subprocess.call(command, stdout=open(file_pfx + ".out", "w"), stderr=open(file_pfx + ".err", "w"), shell=True)
+  rv["out"] = ansi_escape.sub("", open(file_pfx + ".out").read())
+  rv["err"] = ansi_escape.sub("", open(file_pfx + ".err").read())
   return rv
-
-
-
-report_head = """<!DOCTYPE html>
-<html>
-<head>
-  <title>[{0}] Performance Comparison</title>
-  <meta charset="utf-8">
-  <style>
-    body {{
-      font: 10pt sans-serif;
-    }}
-
-    .axis path,
-    .axis line {{
-      fill: none;
-      stroke: #000;
-      shape-rendering: crispEdges;
-    }}
-
-    .d3-tip {{
-      line-height: 1;
-      font-weight: bold;
-      padding: 12px;
-      background: rgba(0, 0, 0, 0.8);
-      color: #fff;
-      border-radius: 2px;
-    }}
-
-    /* Creates a small triangle extender for the tooltip */
-    .d3-tip:after {{
-      box-sizing: border-box;
-      display: inline;
-      font-size: 10px;
-      width: 100%;
-      line-height: 1;
-      color: rgba(0, 0, 0, 0.8);
-      content: "\\25BC";
-      position: absolute;
-      text-align: center;
-    }}
-
-    /* Style northward tooltips differently */
-    .d3-tip.n:after {{
-      margin: -1px 0 0 0;
-      top: 100%;
-      left: 0;
-    }}
-  </style>
-  <script src="http://d3js.org/d3.v3.min.js"></script>
-  <script src="d3-tip.js"></script>
-  <script>
-    function makePerformancePlot(data, title) {{
-
-      var margin = {{top: 40, right: 20, bottom: 30, left: 40}},
-          width = 500 - margin.left - margin.right,
-          height = 300 - margin.top - margin.bottom;
-
-      var x0 = d3.scale.ordinal()
-          .rangeRoundBands([0, width], .1);
-
-      var x1 = d3.scale.ordinal();
-
-      var y = d3.scale.linear()
-          .range([height, 0]);
-
-      var color = d3.scale.category10();
-
-      var xAxis = d3.svg.axis()
-          .scale(x0)
-          .orient("bottom");
-
-      var yAxis = d3.svg.axis()
-          .scale(y)
-          .orient("left")
-          .tickFormat(d3.format(".2s"));
-
-      var svg = d3.select("body").append("p")
-        .append("svg")
-          .attr("width", width + margin.left + margin.right)
-          .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-          .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-      var series = d3.keys(data[0]).filter(function(key) {{ return key !== "Configuration"; }});
-
-      data.forEach(function(d) {{
-        d.ages = series.map(function(name) {{ return {{name: name, value: +d[name]}}; }});
-      }});
-
-      x0.domain(data.map(function(d) {{ return d.Configuration; }}));
-      x1.domain(series).rangeRoundBands([0, x0.rangeBand()]);
-      y.domain([0, d3.max(data, function(d) {{ return d3.max(d.ages, function(d) {{ return d.value; }}); }})]);
-
-      var tip = d3.tip()
-        .attr('class', 'd3-tip')
-        .offset([-10, 0])
-        .html(function(d) {{
-          return "<strong>Time:</strong> " + d.value + " s";
-        }});
-
-      svg.call(tip);
-
-      svg.append("g")
-          .attr("class", "x axis")
-          .attr("transform", "translate(0," + height + ")")
-          .call(xAxis);
-
-      svg.append("g")
-          .attr("class", "y axis")
-          .call(yAxis)
-        .append("text")
-          .attr("transform", "rotate(-90)")
-          .attr("y", 6)
-          .attr("dy", ".71em")
-          .style("text-anchor", "end")
-          .text("run time (s)");
-
-      var configuration = svg.selectAll(".configuration")
-          .data(data)
-        .enter().append("g")
-          .attr("class", "g")
-          .attr("transform", function(d) {{ return "translate(" + x0(d.Configuration) + ",0)"; }});
-
-      configuration.selectAll("rect")
-          .data(function(d) {{ return d.ages; }})
-        .enter().append("rect")
-          .attr("width", x1.rangeBand())
-          .attr("x", function(d) {{ return x1(d.name); }})
-          .attr("y", function(d) {{ return y(d.value); }})
-          .attr("height", function(d) {{ return height - y(d.value); }})
-          .style("fill", function(d) {{ return color(d.name); }})
-          .on("mouseover", tip.show)
-          .on("mouseout", tip.hide);
-
-      svg.append("text")
-          .attr("x", (width / 2))             
-          .attr("y", 0 - (margin.top / 2))
-          .attr("text-anchor", "middle")  
-          .style("font-size", "16px") 
-          .style("text-decoration", "underline")  
-          .text(title);
-
-      var legend = svg.selectAll(".legend")
-          .data(series.slice())
-        .enter().append("g")
-          .attr("class", "legend")
-          .attr("transform", function(d, i) {{ return "translate(0," + i * 20 + ")"; }});
-
-      legend.append("rect")
-          .attr("x", width - 18)
-          .attr("width", 18)
-          .attr("height", 18)
-          .style("fill", color)
-          .style("stroke", "black");
-
-      legend.append("text")
-          .attr("x", width - 24)
-          .attr("y", 9)
-          .attr("dy", ".35em")
-          .style("text-anchor", "end")
-          .text(function(d) {{ return d; }});
-    }}
-  </script>
-</head>
-<body>
-"""
-
 
 if __name__ == "__main__":
   main()
