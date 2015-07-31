b4d1c6c81883bd31221c10947805274eb8339299
dkoeplin <dkoeplinger@gmail.com>
2015-07-31 12:12:41 -0700

    Sync for ASPLOS new ops, transformer updates, and bug fixes

--- asplos-make.sh
+++ asplos-make.sh
@@ -1,1 +1,3 @@
+#sbt -Dtests.threads=1,4 -Dtests.targets=scala -DshowSuppressedErrors=true "; project asplos-apps; compile"
+
 sbt -Dtests.threads=1,4 -Dtests.targets=scala "; project asplos-apps; compile"
