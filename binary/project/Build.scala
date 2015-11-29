b98a5ae20a69cabd56ed40c6fe7237ab9fb1af21
Cédric Bastin <cedricbastin@gmail.com>
2015-11-29 09:56:09 -0800

    forge generates stuff but it's not correct

--- binary/project/Build.scala
+++ binary/project/Build.scala
@@ -3,11 +3,11 @@ import sbt._
 import Keys._
 
 object HUMAN_DSL_NAMEBuild extends Build {
-  val virtScala = "2.10.2"
+  val virtScala = "2.11.2"
 
   val virtBuildSettingsBase = Project.defaultSettings ++ Seq(
     organization := "stanford-ppl",
-    scalaOrganization := "org.scala-lang.FIXME" //virtualized",
+    scalaOrganization := "org.scala-lang" //.virtualized",
     scalaVersion := virtScala,
     publishArtifact in (Compile, packageDoc) := false,
     libraryDependencies += "org.scala-lang.virtualized" % "scala-library" % virtScala,
@@ -23,7 +23,7 @@ object HUMAN_DSL_NAMEBuild extends Build {
     // used in delitec to access jars
     retrieveManaged := true,
     scalacOptions += "-Yno-generic-signatures",
-    scalacOptions += "-Yvirtualize",
+    // scalacOptions += "-Yvirtualize",
     initialCommands in console += "import LOWERCASE_DSL_NAME.library._; val HUMAN_DSL_NAME = new HUMAN_DSL_NAMEREPL { def main() = {} }; import HUMAN_DSL_NAME._"
   )
 
